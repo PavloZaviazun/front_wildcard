f4afb4fb2af257a00cc430c5224a34a73ec6d9b7
Pavlo Zaviazun <help4engineer@gmail.com>
2021-05-16 19:05:44 +0300

    fourth commit PZ 16.05.2021

--- src/components/admin/users/userDetails/UserDetails.js
+++ src/components/admin/users/userDetails/UserDetails.js
@@ -4,11 +4,9 @@ import {commonService, userService} from "../../../../services";
 
 export const UserDetails = (props) => {
     const {user} = props;
-    const ADMIN_EMAIL = "englishwildcard@gmail.com";
 
     const [roles, setRoles] = useState([]);
     const [request, setRequest] = useState("");
-
 
     useEffect(() => {
         commonService.getAllRoles().then(el => setRoles(el));
@@ -22,26 +20,25 @@ export const UserDetails = (props) => {
 
     return (
         <div>
-            {user.email !== ADMIN_EMAIL ?
-                (<form onSubmit={(e) => handleUser(e, user.id)} name={`userForm${user.id}`}>
-                    <div className={"user-details-div"}>
-                        <div className={"user-details-id"}>{user.id}</div>
-                        <div className={"user-details-email"}>{user.email}</div>
-                        <div className={"user-details-nativeLang"}>{user.nativeLang}</div>
-                        <div className={"user-details-role"}>
-                            <select name={"role"}>
-                                <option>{user.roles[0]}</option>
-                                {roles.filter(el => user.roles[0] !== el).map(el => <option key={el}>{el}</option>)}
-                            </select>
-                        </div>
-                        <div className={"user-details-enabled"}>
-                            <input type={"checkbox"} name={"isEnabled"} defaultChecked={user.enabled}/>
-                        </div>
-                        <div>
-                            <button>Submit</button>
-                        </div>
+            <form onSubmit={(e) => handleUser(e, user.id)} name={`userForm${user.id}`}>
+                <div className={"user-details-div"}>
+                    <div className={"user-details-id"}>{user.id}</div>
+                    <div className={"user-details-email"}>{user.email}</div>
+                    <div className={"user-details-nativeLang"}>{user.nativeLang}</div>
+                    <div className={"user-details-role"}>
+                        <select name={"role"}>
+                            <option>{user.roles[0]}</option>
+                            {roles.filter(el => user.roles[0] !== el).map(el => <option key={el}>{el}</option>)}
+                        </select>
                     </div>
-                </form>) : null}
+                    <div className={"user-details-enabled"}>
+                        <input type={"checkbox"} name={"isEnabled"} defaultChecked={user.enabled}/>
+                    </div>
+                    <div>
+                        <button>Submit</button>
+                    </div>
+                </div>
+            </form>
             <div>{request}</div>
         </div>
     )
